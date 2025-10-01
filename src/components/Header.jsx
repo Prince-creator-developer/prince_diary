cb5ba4fe37bf215a4b08457949c1c884939ce032
Prince Kumar <182784546+Prince-creator-developer@users.noreply.github.com>
2025-10-01 10:37:51 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <em>Prince's Diary</em>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
